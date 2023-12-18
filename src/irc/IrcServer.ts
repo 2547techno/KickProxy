37633f82e69c65b08534dbcf0c78e1bd3ff372ce
2547techno <technowizz2547@gmail.com>
2023-12-18 13:45:17 -0500

    store clients' joined channels;create channel-client index

--- src/irc/IrcServer.ts
+++ src/irc/IrcServer.ts
@@ -4,6 +4,7 @@ import { Server, Socket, createServer } from "net";
 interface Client {
     socket: Socket;
     nick: string;
+    channels: Set<string>;
 }
 
 class IrcServer extends EventEmitter {
@@ -11,31 +12,72 @@ class IrcServer extends EventEmitter {
     port: number;
     server: Server;
     clients: Client[];
+    channelMap: Map<string, Set<Client>>;
 
     constructor(host: string, port: number) {
         super();
         this.host = host;
         this.port = port;
-        this.server = createServer(this.handleConnection);
         this.clients = [];
+        this.channelMap = new Map<string, Set<Client>>()
+        this.server = createServer(this.handleConnection.bind(this));
+        setInterval(() => {
+            // console.log(this.clients.map(c => c.channels));
+            // console.log(this.channelMap);
+            for(const client of this.clients) {
+                client.channels.forEach(channel => {
+                    client.socket.write(`:echo PRIVMSG #${channel} :echo${Math.floor(Math.random()*100)}\r\n`)
+                })
+            }
+        }, 2000)
     }
 
     start() {
         this.server.listen(this.port, this.host, () => this.emit("start"))
     }
 
+    joinChannel(client: Client, channel: string) {
+        client.channels.add(channel)
+        if (!this.channelMap.has(channel)) {
+            this.channelMap.set(channel, new Set<Client>())
+        }
+        this.channelMap.get(channel)?.add(client)
+    }
+
+    partChannel(client: Client, channel: string) {
+        client.channels.delete(channel)
+        this.channelMap.get(channel)?.delete(client)
+        if (this.channelMap.get(channel)?.size == 0) {
+            this.channelMap.delete(channel)
+        }
+    }
+
     handleConnection(socket: Socket) {
         console.log(`New client connected: ${socket.remoteAddress}:${socket.remotePort}`);
         const client: Client = {
             socket,
-            nick: "anon"
+            nick: "anon",
+            channels: new Set<string>()
         }
         this.clients.push(client);
 
-        client.socket.write("Connected to proxy!\n");
+        socket.write("Connected to proxy!\r\nType /raw JOIN #channel to connect to a channel!\r\n");
 
-        client.socket.on("data", () => console.log);
-        client.socket.on("end", () => null);
+        socket.on("data", data => {
+            console.log(data.toString())
+            if (data.toString().startsWith("JOIN #")) {
+                const channel = data.toString().substring("JOIN #".length).split(" ")[0].replace(/\r|\n/gi, "")
+                socket.write(`Joining #${channel} ...\r\n`)
+                this.joinChannel(client, channel)
+                socket.write(`Joined #${channel} !\r\n`)
+            } else if (data.toString().startsWith("PART #")) {
+                const channel = data.toString().substring("JOIN #".length).split(" ")[0].replace(/\r|\n/gi, "")
+                socket.write(`Parting #${channel} ...\r\n`)
+                this.partChannel(client, channel)
+                socket.write(`Parted #${channel} !\r\n`)
+            }
+        });
+        socket.on("end", () => null);
     }
 
     stop() {
